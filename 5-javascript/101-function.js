8f3b1486fd3051602e3e81b5e6e81fcd5a7a0771
Leah <leahh2411@gmail.com>
2019-07-12 11:20:10 -0700

    Create 101-function.js

---
+++ 5-javascript/101-function.js
@@ -0,0 +1,10 @@
+var b = myFunction("Hello World")
+function myFunction(b) {
+ console.log(b)
+}
+
+var a = myFunction(prompt("Give me a number"))
+function myFunction(a) {
+let c = a*2
+ console.log(c);
+}
