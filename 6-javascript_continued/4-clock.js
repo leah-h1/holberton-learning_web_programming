185692d66aba82a0ecd55936495f07f005b82624
Leah <leahh2411@gmail.com>
2019-07-15 11:13:44 -0700

    Update 4-clock.js

--- 6-javascript_continued/4-clock.js
+++ 6-javascript_continued/4-clock.js
@@ -1,10 +1,12 @@
 var i = 0;
+var j = 0;
+
 while (i < 24) {
 console.log (i)
   i++;
-}
-var b = 0;
-while (b < 60) {
-  console.log (b)
-    b++;
+
+  while (j < 60) {
+    console.log (j)
+      j++;
+  }
 }
