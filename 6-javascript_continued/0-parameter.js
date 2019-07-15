62937cbc16909b04ded0e4b39be66e7d022d8987
Leah <leahh2411@gmail.com>
2019-07-15 10:02:14 -0700

    Update 0-parameter.js

--- 6-javascript_continued/0-parameter.js
+++ 6-javascript_continued/0-parameter.js
@@ -1,2 +1,2 @@
-let my_parameter = process.argv(2);
-console.log('Here is the first passed parameter: ', my_parameter:);
+let my_parameter = process.argv[2];
+console.log("Here is the first passed parameter: ", my_parameter:);
